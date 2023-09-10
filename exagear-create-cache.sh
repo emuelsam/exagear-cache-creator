519c6a333379f118f21c12996d0a933c84f98c1a
emuelsam <144535017+emuelsam@users.noreply.github.com>
2023-09-10 11:48:06 +0200

    Update exagear-create-cache.sh
    added debootstrap install command 
    
    commented command of remove selected os to save bandwidth

--- exagear-create-cache.sh
+++ exagear-create-cache.sh
@@ -29,14 +29,16 @@ do
 		*)      echo "Invalid option $REPLY";;
 	esac
 done
+echo 'installing debootstrap proot wget'
+sudo apt install debootstrap proot wget
 
 echo 'Starting to download the Ubuntu rootfs...'
 
-sudo mkdir -p $SELECTED_OS
+mkdir -p $SELECTED_OS
 sudo debootstrap --arch=i386 --variant=minbase $SELECTED_OS $SELECTED_OS http://mirror.yandex.ru/ubuntu
 
-sudo cp chroot.sh $SELECTED_OS
-sudo touch vars.txt
+cp chroot.sh $SELECTED_OS
+touch vars.txt
 if [ $SELECTED_OS == "focal" ]
 then
 	printf 'focal' > vars.txt
@@ -44,27 +46,27 @@ elif [ $SELECTED_OS == "xenial" ]
 then
 	printf 'xenial' > vars.txt
 fi
-sudo cp vars.txt $SELECTED_OS
-sudo cd $SELECTED_OS
-sudo mount proc -t proc ./proc
-sudo mount sys -t sysfs ./sys
-sudo mount --bind /dev ./dev
-sudo mount --bind /dev/pts ./dev/pts
-sudo chroot ./ /usr/bin/env -i HOME=/root TERM="$TERM" /bin/bash -l "chroot.sh"
+cp vars.txt $SELECTED_OS
+cd $SELECTED_OS
+mount proc -t proc ./proc
+mount sys -t sysfs ./sys
+mount --bind /dev ./dev
+mount --bind /dev/pts ./dev/pts
+chroot ./ /usr/bin/env -i HOME=/root TERM="$TERM" /bin/bash -l "chroot.sh"
 
-sudo umount dev/pts dev proc sys
-
-cd ..
-
-sudo cp -r cache-fixes/. $SELECTED_OS
-sudo cp -r libs/. $SELECTED_OS
-
-sudo cd $SELECTED_OS
-sudo zip --symlinks -r main.30.com.eltechs.ed.obb .
-sudo cp main.30.com.eltechs.ed.obb ../obb-cache
+umount dev/pts dev proc sys
 
 cd ..
-sudo rm -r $SELECTED_OS
+
+cp -r cache-fixes/. $SELECTED_OS
+cp -r libs/. $SELECTED_OS
+
+cd $SELECTED_OS
+zip --symlinks -r main.30.com.eltechs.ed.obb .
+cp main.30.com.eltechs.ed.obb ../obb-cache
+
+cd ..
+#sudo rm -r $SELECTED_OS
 
 clear
 
